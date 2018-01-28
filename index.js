5cf47c08790a584ffaf8f2dd2d76b3ac66a80ea5
umkafoto <35717972+umkafoto@users.noreply.github.com>
2018-01-29 00:55:57 +1100

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,6 @@
 
 // Подключение модуля
 var TelegramBot = require('node-telegram-bot-api');
-/////////////
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-////////////////
 
 // Токен вашего бота, который можно узнать у @BotFather
 var token = "514827275:AAFlyESPn8KiWuLYGjIAbf6-9ZHzfW5HMfw";
