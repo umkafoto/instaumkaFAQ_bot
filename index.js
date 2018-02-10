e9957fd8d842a07dfdd9cb106e8f3fad05ec51ae
umkafoto <35717972+umkafoto@users.noreply.github.com>
2018-02-11 00:25:40 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// Только сейчас я понял, что сделал не очень правильно, засунув все примеры в один файл
-// Что мешало нормально запускать "простого" бота
-// Поэтому сделаем так - этого бота можно будет запустить без проблем, если вы указали все правильно
-// А уже если вы поняли как это работает, то можете брать примеры код из статьи или файла extended.js и пробовать применить их сюда
-
-// Подключение модуля
-var TelegramBot = require('node-telegram-bot-api');
-
 // Токен вашего бота, который можно узнать у @BotFather
 var token = "514827275:AAFlyESPn8KiWuLYGjIAbf6-9ZHzfW5HMfw";
 
 // Подключаем бота и указываем, что хотим получать сообщения и тд
 var bot = new TelegramBot(token, {
-	polling: true
+  polling: true
 });
 
 // Оповещение о запуске бота
 bot.getMe().then(function (value) {
-	
-	let message = "Запущен бот @" + value['username'];
-	// Вариант с логом в консоль
-	console.log(message);
+  
+  let message = "Запущен бот @" + value['username'];
+  // Вариант с логом в консоль
+  console.log(message);
 
 });
 
 // Команда /start
 bot.onText(/\/start/, (msg, match) => {
-	// Текст сообщения
-	let message = ", привет, я бот помощник канала Инстаграмотность.\n\nТы можешь задать мне вопрос, и Илья рассмотрит его в одном из следующих выпусков рубрики ответы на вопросы.\n\nЕсли вопроса нет, переходи и читай @INSTAumka";
-	// Отправка сообщения
-	bot.sendMessage(msg.chat.id,msg.from.first_name + message);
+  // Текст сообщения
+  let message = ", привет, я бот помощник канала Инстаграмотность.\n\nТы можешь задать мне вопрос, и Илья рассмотрит его в одном из следующих выпусков рубрики ответы на вопросы.\n\nЕсли вопроса нет, переходи и читай @INSTAumka";
+  // Отправка сообщения
+  bot.sendMessage(msg.chat.id,msg.from.first_name + message);
+  
+});
+
+bot.onText(/\/help/, (msg, match) => {
+  // Текст сообщения
+  let message = ""Бот канала Инстаграмотность. Здесь вы можете задать свои вопросы по Instagram, и в одном из выпуском получить отвт на них"";
+  // Отправка сообщения
+  bot.sendMessage(msg.chat.id,msg.from.first_name + message);
   
 });
 
 bot.on('message', (msg) => {
     console.log(msg);
-if (msg.text != '/start') {
+if (msg.text != '/start' && msg.text!='/help') {
     bot.forwardMessage(245217438, msg.chat.id, msg.message_id);
     bot.sendMessage(msg.chat.id, msg.from.first_name + ', спасибо за вопрос. Мы его обязательно рассмотрим!');
 }
